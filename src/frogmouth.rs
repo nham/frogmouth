f2fef774a80c21b943b81cbac4a432dcc6351916
nham <hamann.nick@gmail.com>
2014-07-08 11:05:52 -0400

    I have done battle with the borrow checker and prevailed. This commit completes an implementation of ConcatParser

--- src/frogmouth.rs
+++ src/frogmouth.rs
@@ -25,20 +25,30 @@ enum ParseTree<T> {
     Concat(Box<ParseTree<T>>, Box<ParseTree<T>>),
 }
 
+impl<T: Clone> Clone for ParseTree<T> {
+    fn clone(&self) -> ParseTree<T> {
+        match *self {
+            Nil => Nil,
+            Leaf(ref v) => Leaf(v.clone()),
+            Alt(ref a, ref b) => Alt(a.clone(), b.clone()),
+            Concat(ref a, ref b) => Concat(a.clone(), b.clone()),
+        }
+    }
+}
+
 struct ConcatParser<P, Q> {
     p1: P,
     p2: Q,
 }
 
-impl<S, T, P: Parser<S,ParseTree<T>>, Q: Parser<S,ParseTree<T>>> Parser<S,ParseTree<T>> for ConcatParser<P, Q> {
+impl<S, T: Clone, P: Parser<S,ParseTree<T>>, Q: Parser<S,ParseTree<T>>> Parser<S,ParseTree<T>> for ConcatParser<P, Q> {
     fn parse<'a>(&self, state: &'a [S]) -> ParseResult<'a, S, ParseTree<T>> {
-        let mut p1_parse = self.p1.parse(state);
+        let mut p1_parse: Vec<(&'a [S], ParseTree<T>)> = self.p1.parse(state);
 
         let mut out = vec!();
         for (rem, tree) in p1_parse.move_iter() {
             for (rem2, tree2) in self.p2.parse(rem).move_iter() {
-                let boxtree = box tree;
-                out.push((rem2, Concat(boxtree, box tree2)));
+                out.push((rem2, Concat(box tree.clone(), box tree2)));
             }
         }
         out
